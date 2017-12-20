b7d30d72f973497aeb18f769c75478c34bf9c5e5
RyoHaru <ryo180xion@gmail.com>
2017-12-20 13:31:11 +0900

    修正

--- formatschedule.js
+++ formatschedule.js
@@ -1,103 +1,78 @@
+var formatSchedule = function(xml){
 
-	// 通常予定
+	// 通常予定の日時処理
 	var getNormalSche = function(input){
-		var date = new Date(input.value);
-		var year = date.getFullYear();
-		var month = date.getMonth() + 1;
-		var day = date.getDate();
-		var hour = date.getHours();
-		var minute = date.getMinutes();
-		var time = hour + ":" + minute;
-		// console.log(time);
+        var date = new Date(input.value);
+        var hour = ("0" + date.getHours()).slice(-2);
+		var minute = ("0" + date.getMinutes()).slice(-2);
+        var time = hour + ":" + minute;
+        // 以下、日付部分(今後機能追加で使うかも)
+		// var year = date.getFullYear();
+		// var month = ("0" + (date.getMonth() + 1)).slice(-2);
+		// var day = ("0" + date.getDate()).slice(-2)
 		return time;
 	};
-	// 繰り返し予定
+	// 繰り返し予定の日時処理
 	var getRepeatSche = function(input){
-		var date = new Date();
-		var year = date.getFullYear();
-		var month = date.getMonth() + 1;
-		var day = date.getDate();
-		// console.log(input.value);
-		return input.value;
+        return input.value.slice(0,-3);
+        // 以下、日付部分(今後機能追加で使うかも)
+		// var date = new Date();
+		// var year = date.getFullYear();
+		// var month = date.getMonth() + 1;
+        // var day = date.getDate();
 	};
-	// 帯予定
-	var getRepeatSche = function(input){
-		// console.log(input.value);
-		return input.value;
-	};
-	// var getSchedule = function(xml) {
-    //     // スケジュールタイプで判断
-    //     switch (schedule[index].event_type){
-    //         case "normal":
-    //             console.log("通常予定です。")
-    //             if($(this).children().children("datetime")[0] == undefined)break;
-    //             var value = $(this).children().children("datetime")[0].attributes;
-    //             schedule[index].start_time = getNormalSche(value["start"]);
-    //             schedule[index].end_time = getNormalSche(value["end"]);
-    //             console.log(schedule[index].start_time)
-    //             console.log(schedule[index].end_time);
-    //             break;
-    //         case "repeat":
-    //             console.log("繰り返し予定です。");
-    //             var value = $(this).children().children("condition")[0].attributes;
-    //             schedule[index].start_time = getRepeatSche(value["start_time"]);
-    //             schedule[index].end_time = getRepeatSche(value["end_time"]);
-    //             console.log(schedule[index].start_time);
-    //             console.log(schedule[index].end_time);
-    //             break;
-    //         case "banner":
-    //             console.log("帯予定です。");
-    //             var value = $(this).children().children("date")[0].attributes;
-    //             schedule[index].start_time = null;
-    //             schedule[index].end_time = null;
-    //             console.log(schedule[index].start_time);
-    //             console.log(schedule[index].end_time);
-    //             console.log(schedule[index].detail);
-    //             break;
-    //     }
-    // };
-    var formatSchedule = function(xml) {
-        var schedule = new Array();
-		var index = 0;
-		$(xml).find("schedule_event").each(function () {
-			schedule[index] = new Object();
-			schedule[index].id = $(this).attr("id");
-			schedule[index].event_type = $(this).attr("event_type");
-			schedule[index].public_type = $(this).attr("public_type");
-			schedule[index].plan = $(this).attr("plan");
-			schedule[index].detail = $(this).attr("detail");
-			schedule[index].version = $(this).attr("version");
-			schedule[index].all_day = $(this).attr("allday");
-            schedule[index].start_only = $(this).attr("start_only");
-            switch (schedule[index].event_type){
-                case "normal":
-                    console.log("通常予定です。")
-                    if($(this).children().children("datetime")[0] == undefined)break;
-                    var value = $(this).children().children("datetime")[0].attributes;
-                    schedule[index].start_time = getNormalSche(value["start"]);
-                    schedule[index].end_time = getNormalSche(value["end"]);
-                    console.log(schedule[index].start_time)
-                    console.log(schedule[index].end_time);
+    // スケジュールのタイプが通常/繰り返し/帯で分岐させる
+    var checkScheduleType = function(array, count) {
+        switch (array.event_type){
+            case "normal":
+                console.log("通常予定です。")
+                //終日予定の場合の分岐
+                if(count.children().children("datetime")[0] == undefined) {
                     break;
-                case "repeat":
-                    console.log("繰り返し予定です。");
-                    var value = $(this).children().children("condition")[0].attributes;
-                    schedule[index].start_time = getRepeatSche(value["start_time"]);
-                    schedule[index].end_time = getRepeatSche(value["end_time"]);
-                    console.log(schedule[index].start_time);
-                    console.log(schedule[index].end_time);
-                    break;
-                case "banner":
-                    console.log("帯予定です。");
-                    var value = $(this).children().children("date")[0].attributes;
-                    schedule[index].start_time = null;
-                    schedule[index].end_time = null;
-                    console.log(schedule[index].start_time);
-                    console.log(schedule[index].end_time);
-                    console.log(schedule[index].detail);
-                    break;
-            }
-			index = index + 1;
-        });
-        return schedule;
+                }
+                var value = count.children().children("datetime")[0].attributes;
+                array.start_time = getNormalSche(value["start"]);
+                array.end_time = getNormalSche(value["end"]);
+                break;
+            case "repeat":
+                console.log("繰り返し予定です。");
+                var value = count.children().children("condition")[0].attributes;
+                array.start_time = getRepeatSche(value["start_time"]);
+                array.end_time = getRepeatSche(value["end_time"]);
+                break;
+            case "banner":
+                console.log("帯予定です。");
+                break;
+        }
     };
+    // スケジュールのソート
+    var sortTime = function(array){
+        array.sort(function(a,b){
+            if( a.start_time > b.start_time ) return 1;
+            if( a.start_time < b.start_time ) return -1;
+            return 0;
+        });
+        return array;
+    }
+    // メインの処理
+    var setSchedule = function(){
+        var schedule = new Array();
+        var index = 0;
+        $(xml).find("schedule_event").each(function () {
+            schedule[index] = new Object();
+            schedule[index].id = $(this).attr("id");
+            schedule[index].event_type = $(this).attr("event_type");
+            schedule[index].public_type = $(this).attr("public_type");
+            schedule[index].plan = $(this).attr("plan");
+            schedule[index].detail = $(this).attr("detail");
+            schedule[index].version = $(this).attr("version");
+            schedule[index].all_day = $(this).attr("allday");
+            schedule[index].start_only = $(this).attr("start_only");
+            checkScheduleType(schedule[index],$(this))
+            index = index + 1;
+        });
+        return sortTime(schedule);
+    }
+    var schedule = setSchedule();
+    return schedule;
+}
